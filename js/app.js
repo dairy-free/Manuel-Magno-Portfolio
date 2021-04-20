170d561ddb959594860542e27c716dc99f1479db
dairy-free <67081832+dairy-free@users.noreply.github.com>
2021-04-20 02:11:05 -0700

    portfolio

--- js/app.js
+++ js/app.js
@@ -1,19 +1,25 @@
 const navBar = document.getElementsByClassName('navbar-container')[0]
 const menuBtn = document.getElementsByClassName('menu-btn')[0]
 let menuOpen = false;
+const showNav = document.getElementsByClassName('navbar-container')[0]
+console.log(showNav)
 
+// Menu Class
 menuBtn.addEventListener('click', () => {
   if(!menuOpen) {
     menuBtn.classList.add('open');
+    navBar.classList.add('show-nav')
     menuOpen = true;
+    
   } else {
     menuBtn.classList.remove('open');
+    navBar.classList.remove('show-nav')
     menuOpen = false;
   }
-})
+});
 
-// console.log(menuBtn)
 
+// Toggle Nav Menu
 // toggleNav.addEventListener('click', () => navBar.classList.add('show-nav'));
 
 
