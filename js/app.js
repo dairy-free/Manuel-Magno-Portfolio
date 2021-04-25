f655636c3787be13b38efa7fffdd79abc606205a
dairy-free <67081832+dairy-free@users.noreply.github.com>
2021-04-25 07:42:51 -0700

    portfolio

--- js/app.js
+++ js/app.js
@@ -2,7 +2,11 @@ const navBar = document.getElementsByClassName('navbar-container')[0]
 const menuBtn = document.getElementsByClassName('menu-btn')[0]
 let menuOpen = false;
 const showNav = document.getElementsByClassName('navbar-container')[0]
-const darkenContainer = document.getElementsByClassName('container')[0]
+const darkenContainer = document.getElementsByClassName('darken-container')[0]
+const body = document.getElementsByTagName('body')[0]
+console.log(body)
+
+
 
 
 // Toggles Nav Menu & Transforms Hamburger Menu
@@ -11,12 +15,14 @@ menuBtn.addEventListener('click', () => {
     menuBtn.classList.add('open');
     navBar.classList.add('show-nav')
     darkenContainer.classList.add('darken')
+    body.style.overflow = "hidden";
     menuOpen = true;
     
   } else {
     menuBtn.classList.remove('open');
     navBar.classList.remove('show-nav')
     darkenContainer.classList.remove('darken')
+    body.style.overflow = "visible";
     menuOpen = false;
   }
 });
