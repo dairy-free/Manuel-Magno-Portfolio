6b4430316ecbdf25122d7b1c42d4663fdc32d48b
dairy-free <67081832+dairy-free@users.noreply.github.com>
2021-05-22 03:01:31 -0700

    portfolio

--- js/app.js
+++ js/app.js
@@ -11,6 +11,7 @@ const body = document.getElementsByTagName('body')[0]
 
 
 
+
 // Toggles Nav Menu & Transforms Hamburger Menu
 menuBtn.addEventListener('click', NavMenuToggle);
 navWork.addEventListener('click', NavMenuToggle);
@@ -35,5 +36,36 @@ function NavMenuToggle(){
     menuOpen = false;
   }
 }
+
+
+// Observer 
+
+const sections = document.querySelectorAll('.sections');
+const introSection = document.getElementById('introduction');
+const workSection = document.getElementById('work');
+const aboutSection = document.getElementById('about-me');
+const contactSection = document.getElementById('contact-me');
+
+
+
+
+const options = {
+  threshold: 0,
+  rootMargin: "-150px"
+};
+
+const observer = new IntersectionObserver(function(entries, observer ) {
+  entries.forEach(entry => {
+    // console.log(entry.target)
+    // sections.classList.add('fade-in')
+
+  })
+}, options);
+
+sections.forEach(section => {
+    observer.observe(section);
+});
+
+// observer.observe(introSection)
 
 
